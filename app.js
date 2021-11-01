9e44cc475daccdb76eb0d4f17ba8d2427bbdb4a3
Michaela Wright <75323745+Mich-moon@users.noreply.github.com>
2021-11-01 00:47:33 -0500

    progress

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 window.onload = function() {
 
     let btn = document.getElementById("myButton");
+    let search = document.getElementsByTagName("input")[0];
+    let result = document.getElementById("result");
+
     btn.addEventListener("click", function(element){
         element.preventDefault();
 
@@ -16,9 +19,21 @@ window.onload = function() {
                 }
             })
             .then(data => {
-                alert(data);
+                displayResult(data);
+                //alert(data);
             })
             .catch(error => console.log(error));
 
     });
+
+    let displayResult = function(data) {
+        let searchText = search.value.trim();
+        let exp = /[a-zA-Z] /;   // regular expression for lowercase and uppercase letters
+
+        if (searchText.length == 0) {
+            result.innerHTML = data;
+        } else if (exp.test(searchText)) {
+
+        }
+    };
 };
