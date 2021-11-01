c8d4e1e0b81d091a32a3fe60e85a33bead3d4298
Michaela Wright <75323745+Mich-moon@users.noreply.github.com>
2021-11-01 02:36:56 -0500

    prog

--- app.js
+++ app.js
@@ -8,32 +8,52 @@ window.onload = function() {
 
     btn.addEventListener("click", function(element){
         element.preventDefault();
+        let searchText = search.value.trim();
+        let exp = /[a-zA-Z] /;   // regular expression for lowercase and uppercase letters
 
-        //Fetch the data by opening an AJAX request which returns the result of superheroes.php.
-        fetch("superheroes.php")
+        // remove previous result
+        result.innerHTML = "";
+
+        if (searchText.length == 0) {
+            //Fetch the data by opening an AJAX request which returns the result of superheroes.php.
+            fetch("superheroes.php")
             .then(response => {
                 if (response.ok) {
                     return response.text()
                 } else {
-                    return Promise.reject('something went wrong!')
+                    return Promise.reject('could not fetch data')
                 }
             })
             .then(data => {
+                // show result on page
                 displayResult(data);
-                //alert(data);
             })
             .catch(error => console.log(error));
+            
+
+        } else if (exp.test(searchText)) {
+            //Fetch a hero that matches the search text
+            let url = "superheroes.php?query="+searchText ;
+            fetch(url)
+            // fetch("superheroes.php?parameter=hero")
+            .then(response => {
+                if (response.ok) {
+                    return response.json()
+                } else {
+                    return Promise.reject('could not fetch data')
+                }
+            })
+            .then(heroData => {
+                // show result on page
+                displayResult(heroData);
+            })
+            .catch(error => console.log(error));
+        }
 
     });
 
+    // adds results to the results div
     let displayResult = function(data) {
-        let searchText = search.value.trim();
-        let exp = /[a-zA-Z] /;   // regular expression for lowercase and uppercase letters
-
-        if (searchText.length == 0) {
-            result.innerHTML = data;
-        } else if (exp.test(searchText)) {
-
-        }
+        result.innerHTML = data;
     };
 };
